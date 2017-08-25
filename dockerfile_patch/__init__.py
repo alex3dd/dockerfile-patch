1c4dd5dd9c9a3b5f0bccfa2027d8f8daddb9dab4
Asher256 <asher256@gmail.com>
2017-08-25 18:28:44 -0400

    Tip removed

--- dockerfile_patch/__init__.py
+++ dockerfile_patch/__init__.py
@@ -458,11 +458,11 @@ def main():
         with open(args.output, 'w') as fhandler:
             fhandler.write(output)
 
-        sys.stderr.write('\n[TIP] You can build it with: docker build -f '
-                         + args.output + ' -t ' +
-                         os.path.basename(os.path.abspath(dockerfile_dir)) +
-                         ':latest ' +
-                         dockerfile_dir + '\n')
+        # sys.stderr.write('\n[TIP] You can build it with: docker build -f '
+        #                  + args.output + ' -t ' +
+        #                  os.path.basename(os.path.abspath(dockerfile_dir)) +
+        #                  ':latest ' +
+        #                  dockerfile_dir + '\n')
     else:
         sys.stderr.write('[SUCCESS] Patched Dockerfile:\n')
 
